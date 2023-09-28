84682457410ce5e574cb15ec6f477af2580c1053
Mohamed Hassan <123171342+MOHAMED-HASSAN-1996@users.noreply.github.com>
2023-09-29 01:10:40 +0300

    Add files via upload

---
+++ j.s/mian.js
@@ -0,0 +1,17 @@
+// console.log("%c HELLO WORLID", "color:#052; background:#ee59");
+// var x = 25 ;
+// console.log(x);
+// console.log(typeof x);
+// // number
+// x = "hello" ;
+// console.log(x);
+// console.log(typeof x);
+// // String
+// x = false ; 
+// console.log(x) ;
+// console.log ( typeof x ) ;
+// // Boolean
+// x = null ;
+// console.log(x) ;
+// console.log ( typeof x ) ;
+// null
